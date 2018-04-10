78c54bb1728d83140b70dce8f96c254987b74750
JINHEE YOON <JINY@JINHEEs-MacBook-Pro.local>
2018-04-10 12:58:14 -0700

    add time and weekday factors

--- talking.py
+++ talking.py
@@ -9,9 +9,14 @@ from sklearn.model_selection import train_test_split
 
 
 df = pd.read_csv('train_sample.csv')
-df = df.drop(columns=['click_time','attributed_time'], axis=1)
+df = df.drop(columns=['attributed_time'], axis=1)
+df['click_time'] = pd.to_datetime(df['click_time'])
+df['weekday'] = df['click_time'].dt.dayofweek
+df['hour'] = df['click_time'].dt.hour
 y = df['is_attributed']
-df = df.iloc[:,:-1]
+df = df.drop(columns=['click_time','is_attributed'], axis=1)
+print(df.head())
+
 
 x_train, x_val, y_train, y_val = train_test_split(df,y,test_size=0.2)
 # x_train = x_train.transpose()
@@ -19,21 +24,24 @@ x_train, x_val, y_train, y_val = train_test_split(df,y,test_size=0.2)
 # y_train = y_train.transpose()
 # y_val = y_val.transpose()
 print(x_train.shape,x_val.shape,y_train.shape,y_val.shape)
-print(y.len() - y.sum())
-class_weight = {0:1., 1:y.size() - y.sum()}
+print(y.size - y.sum())
+class_weight = {0:1., 1:900}
 del df, y
 
 model = Sequential()
-model.add(Dense(512,activation='relu',input_shape=(5,)))
+model.add(Dense(512,activation='relu',input_shape=(x_train.shape[1],)))
 model.add(Dropout(0.2))
 model.add(Dense(512,activation='relu'))
 model.add(Dropout(0.2))
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.2))
+
 model.add(Dense(1,activation='sigmoid'))
 
-model.compile(loss='binary_crossentropy',optimizer='sgd',metrics=['accuracy'])
-model.fit(x_train,y_train,batch_size=512,epochs=5,validation_data=(x_val,y_val),class_weight=class_weight)
+model.compile(loss='binary_crossentropy',optimizer='Nadam',metrics=['accuracy'])
+model.fit(x_train,y_train,batch_size=10000,epochs=5,validation_data=(x_val,y_val),class_weight=class_weight)
 
-score, acc = model.evaluate(x_val,y_val,batch_size=32)
+score, acc = model.evaluate(x_val,y_val,batch_size=1024)
 print('Test score:',score)
 print('Test accuracy:',acc)
 
@@ -41,12 +49,14 @@ print('Test accuracy:',acc)
 #
 df_sub = pd.DataFrame()
 #
-x_test = pd.read_csv('test.csv')
-df_sub['click_id'] = x_test['click_id']
-x_test = x_test.drop(columns=['click_id','click_time'], axis=1)
-df_sub['is_attributed'] = model.predict(x_test,batch_size=512,verbose=0)
+result_out = False
+if result_out == True:
+    x_test = pd.read_csv('test.csv')
+    df_sub['click_id'] = x_test['click_id']
+    x_test = x_test.drop(columns=['click_id','click_time'], axis=1)
+    df_sub['is_attributed'] = model.predict(x_test,batch_size=512,verbose=0)
 
-print(df_sub['is_attributed'].sum())
-# print(preds)
+    print(df_sub['is_attributed'].sum())
+    # print(preds)
 
-df_sub.to_csv('result1.csv')
+    df_sub.to_csv('result1.csv')
